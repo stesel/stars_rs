54d9764f1d2af6a4b2658473da7c55d63f84b6f4
Leonid Trofymchuk <leonid.trofymchuk@workdayinternal.com>
2022-05-25 23:52:00 +0300

    Add web version

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 ![Rust](https://github.com/stesel/stars_rs/workflows/Rust/badge.svg?branch=bevy)
 # stars_rs
 Star shooter with bevy/wgpu
+
+
+## Run on WEB
+
+Add web assembly target:
+```
+rustup target add wasm32-unknown-unknown
+```
+
+Add wasm-bindgen-cli:
+```
+cargo install -f wasm-bindgen-cli
+```
+
+Build project:
+```
+cargo build --release --target wasm32-unknown-unknown
+wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/release/stars_rs.wasm
+```
--- src/window/mod.rs
+++ src/window/mod.rs
@@ -4,11 +4,13 @@ pub struct WindowPlugin;
 
 impl Plugin for WindowPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(WindowDescriptor {
-            width: 600.0,
-            height: 500.0,
-            title: "stars_rs".to_string(),
-            ..default()
-        });
+        app
+            .insert_resource(WindowDescriptor {
+                width: 600.0,
+                height: 500.0,
+                title: "stars_rs".to_string(),
+                ..default()
+            })
+            .insert_resource(ClearColor(Color::BISQUE));
     }
 }
