370973eaed83b002dfec5b6218b0cec2c5edb433
Leonid Trofymchuk <leonid.trofymchuk@workdayinternal.com>
2022-10-25 12:47:49 +0300

    Add audio context fix

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -7,9 +7,10 @@ pub struct WindowPlugin;
 impl Plugin for WindowPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(WindowDescriptor {
+            canvas: Some("#bevy".to_owned()),
             width: WINDOW_SIZE.width,
             height: WINDOW_SIZE.height,
-            title: "stars_rs".to_string(),
+            title: "stars_rs".to_owned(),
             cursor_visible: false,
             ..default()
         })
